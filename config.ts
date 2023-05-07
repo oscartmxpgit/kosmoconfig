cef28376c07c7a8fbb708f6b019a411bc8136afe
oscartmxpgit <92880098+oscartmxpgit@users.noreply.github.com>
2023-05-07 22:58:35 +0200

    Delete config.ts

--- config.ts
+++
@@ -1,6 +0,0 @@
-export const configSpreadSheet = {
-    operacion: 'H61',
-    operacionNombreCompleto: 'H61: Operación Ícaro',
-    destinatario1: 'Conferencia Obispos Católicos de Cuba',
-    destinatario2: 'P. Charles Monagal (Diocésis de La Habana)',
-  };
