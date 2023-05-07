2a109f96ec72c4543bad772bcce7465486f42585
oscartmxpgit <92880098+oscartmxpgit@users.noreply.github.com>
2023-05-07 22:44:22 +0200

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,6 @@
+export const configSpreadSheet = {
+    operacion: 'H61',
+    operacionNombreCompleto: 'H61: Operación Ícaro',
+    destinatario1: 'Conferencia Obispos Católicos de Cuba',
+    destinatario2: 'P. Charles Monagal (Diocésis de La Habana)',
+  };
